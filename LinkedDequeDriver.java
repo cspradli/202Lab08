56e825c0a4776d7b03568d3d41c0de98954e6df1
Caleb Spradlin <calebspradlin@dhcp-86-174.ram.secure.int.wireless.unca.edu>
2018-10-30 16:23:35 -0400

    made final tests, works

--- LinkedDequeDriver.java
+++ LinkedDequeDriver.java
@@ -2,21 +2,19 @@ public class LinkedDequeDriver{
     public static void main(String[] args) {
         LinkedDeque<String> list = new LinkedDeque<>();
         list.addFirst("James");
-        list.printForward();
         list.addFirst("Bill");
-        list.printForward();
         list.addFirst("Cromwell");
+        list.addFirst("Binky");
+        list.addFirst("Popo");
         list.printForward();
         list.removeFirst();
-        list.printForward();
-        list.addFirst("Peter");
         list.printForward();
         System.out.println("First in list:\n"+list.getFirst()+"\n");
         list.removeFirstOccurrence("Bill");
         list.printForward();
         list.addLast("Ben");
-        list.printForward();
         list.addLast("Bernard");
+        list.addLast("Bell");
         list.printForward();
         list.removeLast();
         list.printForward();
